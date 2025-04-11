692e3b591817c55f2b2b0cc78731733ffa1a953e
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-11 15:46:03 -0400

    Update main.java

--- Notes/main.java
+++ Notes/main.java
@@ -2,11 +2,13 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
 
+    // Comment
     int numOne = 1;
     char singleQuoted = 'A';
     String doubleQuoted = "A";
     boolean true_ = true
-    
+    /* Multiline 
+      Comment */
     System.out.print(number);
     System.out.println()
     System.out.println(greeting);
