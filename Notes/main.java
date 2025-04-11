379d9f1b72d58da3346c24fb3c7902f32f280041
Kurt <150044622+kcrescano@users.noreply.github.com>
2025-04-11 15:20:09 -0400

    Create main.java

---
+++ Notes/main.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+
+    int numOne = 1;
+    char singleQuoted = 'A';
+    String doubleQuoted = "A";
+    
+    System.out.print(number);
+    System.out.println()
+    System.out.println(greeting);
+  }
+}
